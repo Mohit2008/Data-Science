962b6888d24aaa682a6468f971035f7a6574cad0
Mohit Khanna <mohit.khanna2008@gmail.com>
2019-01-29 14:54:27 +0530

    Update sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -81,3 +81,97 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
 
     plt.legend(loc="best")
     return plt
+*********************************************************************************************
+2. Plot confusion matrix
+  
+# confusion matrix 
+def plot_confusion_matrix(cm, classes,
+                          normalize = False,
+                          title = 'Confusion matrix"',
+                          cmap = plt.cm.Blues) :
+    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation = 0)
+    plt.yticks(tick_marks, classes)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])) :
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment = 'center',
+                 color = 'white' if cm[i, j] > thresh else 'black')
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+    
+# Logistic regression 
+log_cfl = LogisticRegression()
+
+log_cfl.fit(X_train, y_train)
+y_pred = log_cfl.predict(X_test)
+y_score = log_cfl.decision_function(X_test)  
+
+# Confusion maxtrix & metrics
+cm = confusion_matrix(y_test, y_pred)
+class_names = [0,1]
+plt.figure()
+plot_confusion_matrix(cm, 
+                      classes = class_names, 
+                      title = 'LOG Confusion matrix')
+plt.show()
+   
+*********************************************************************************************
+3. Plot precision-recall curve
+  
+  # precision-recall curve
+def plot_precision_recall(recall,precision ):
+    plt.step(recall, precision, color = 'b', alpha = 0.2,
+             where = 'post')
+    plt.fill_between(recall, precision, step ='post', alpha = 0.2,
+                 color = 'b')
+
+    plt.plot(recall, precision, linewidth=2)
+    plt.xlim([0.0,1])
+    plt.ylim([0.0,1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision Recall Curve')
+    plt.show();
+ 
+y_pred = log_cfl.predict(X_test)
+y_score = log_cfl.decision_function(X_test)  # get the value from the classifier function
+precision, recall, thresholds = precision_recall_curve(y_test, y_score)
+plot_precision_recall(recall,precision)
+*********************************************************************************************    
+4. Plot ROC curve
+
+# ROC curve
+def plot_roc(fpr,tpr ):
+    plt.plot(fpr, tpr, label = 'ROC curve', linewidth = 2)
+    plt.plot([0,1],[0,1], 'k--', linewidth = 2)
+    plt.xlim([0.0,0.001])
+    plt.ylim([0.0,1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.show();
+ 
+y_pred = log_cfl.predict(X_test)
+y_score = log_cfl.decision_function(X_test)  
+fpr, tpr, t = roc_curve(y_test, y_score)
+plot_roc(fpr,tpr)
+*********************************************************************************************  
+5. Plot feature importance
+
+#feature importance plot
+def plot_feature_importance(model):
+    tmp = pd.DataFrame({'Feature': predictors, 'Feature importance': model.feature_importances_})
+    tmp = tmp.sort_values(by='Feature importance',ascending=False)
+    plt.figure(figsize = (15,8))
+    plt.title('Features importance',fontsize=14)
+    s = sns.barplot(x='Feature',y='Feature importance',data=tmp)
+    s.set_xticklabels(s.get_xticklabels(),rotation=90)
+    plt.show()
