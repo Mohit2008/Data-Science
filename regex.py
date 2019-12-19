21dc797a0f95c9fedf9c446eb8ef3cc9dd407a15
Mohit Khanna <mohit.khanna2008@gmail.com>
2019-12-19 12:52:27 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -23,3 +23,24 @@ digits = r"\d+"
 print(re.findall(digits, my_string)) # Find all digits in my_string and print the result
 Output - ['4', '19']
 
+# To search the presence or absence of some pattern in your string you can use the re.match or re.search methods. re.search will
+#try to look for pattern in the entire string where as re.match only looks at the presence of that pattern at the start 
+#of the string
+
+match = re.search("coconuts", scene_one)
+match = re.match("coconuts", scene_one)
+
+
+# In regex you can define a group using the () brackets
+# Explicit charated ranges can be provided in [] brackets
+# OR is represented using | charater 
+
+match_digits_and_words_pattern= ('(\d+|\w+)')
+re.findall(match_digits_and_words_pattern, "He has 11 cats.")
+Output-  ['He', 'has', '11', 'cats']
+
+# [a-zA-Z] upper and lower case alphabets
+# [0-9] numbers 0 to 9
+# [a-zA-Z\-\.] upper and lower case alphabets and - and .
+#(a-z) a , - , z
+# (\s+|,) space or comma
