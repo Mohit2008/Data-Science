923557e38f1f03148e1c2c5c0f29df8a105e9d8d
Mohit Khanna <mohit.khanna2008@gmail.com>
2019-12-29 13:41:59 +0530

    Update time_series.py

--- time_series.py
+++ time_series.py
@@ -73,6 +73,68 @@ series = read_csv('daily-total-female-births.csv', header=0, parse_dates=[0], in
 
 
 
+# fix the problem of missing value in time series
+
+# Create a function we'll use to interpolate and plot
+def interpolate_and_plot(prices, interpolation):
+
+    # Create a boolean mask for missing values
+    missing_values = prices.isna()
+
+    # Interpolate the missing values
+    prices_interp = prices.interpolate(interpolation)
+
+    # Plot the results, highlighting the interpolated values in black
+    fig, ax = plt.subplots(figsize=(10, 5))
+    prices_interp.plot(color='k', alpha=.6, ax=ax, legend=False)
+    
+    # Now plot the interpolated values on top in red
+    prices_interp[missing_values].plot(ax=ax, color='r', lw=3, legend=False)
+    plt.show()
+
+
+interpolation_type = 'linear' # you can also use 'quadratic' or 'zero'
+interpolate_and_plot(prices, interpolation_type)
+
+
+#############################################
+
+
+# Identify outliers by calculating the percent change of values as a function of past few values 
+
+# Your custom function
+def percent_change(series):
+    # Collect all *but* the last value of this window, then the final value
+    previous_values = series[:-1]
+    last_value = series[-1]
+
+    # Calculate the % difference between the last value and the mean of earlier values
+    percent_change = (last_value - np.mean(previous_values)) / np.mean(previous_values)
+    return percent_change
+
+# Apply your custom function and plot
+prices_perc = prices.rolling(20).apply(percent_change)
+prices_perc.loc["2014":"2015"].plot()
+plt.show()
+
+
+def replace_outliers(series):
+    # Calculate the absolute difference of each timepoint from the series mean
+    absolute_differences_from_mean = np.abs(series - np.mean(series))
+    # Calculate a mask for the differences that are > 3 standard deviations from zero
+    this_mask = absolute_differences_from_mean > (np.std(series) * 3)
+    # Replace these values with the median accross the data
+    series[this_mask] = np.nanmedian(series)
+    return series
+
+# Apply your preprocessing function to the timeseries and plot the results
+prices_perc = prices_perc.apply(replace_outliers)
+prices_perc.loc["2014":"2015"].plot()
+plt.show()
+
+############################################
+
+
 # creating features for time series data
 series = read_csv('daily-total-female-births.csv', header=0, parse_dates=[0], index_col=0,squeeze=True)
 dataframe = DataFrame()
